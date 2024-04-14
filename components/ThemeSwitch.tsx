aba9d300fb89c817ba5f74a135d5ff69cca51a49
Aira Jena <airajena.hack@gmail.com>
2024-04-14 11:22:48 +0530

    feat: Implement light and dark mode functionality

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 
 const ThemeSwitch = () => {
-  return (
-    <button>Theme Switch</button>
-  )
-}
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
-export default ThemeSwitch
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <button
+      className="border"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      {theme === "dark" ? "Light" : "Dark"}
+    </button>
+  );
+};
+
+export default ThemeSwitch;
