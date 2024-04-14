4a52aa3a9f10132ccbbb33cf8c765313c63ed1ff
Aira Jena <airajena.hack@gmail.com>
2024-04-14 12:02:07 +0530

    feat: Add icons for theme switch

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
+import { MoonIcon, SunIcon } from "./Icons";
 
 const ThemeSwitch = () => {
   const { theme, setTheme } = useTheme();
@@ -17,10 +18,10 @@ const ThemeSwitch = () => {
 
   return (
     <button
-      className="border"
+      className="border border-purple-500 rounded-2xl p-1 hover:bg-purple-500 hover:bg-opacity-10 dark:hover:bg-opacity-10 dark:hover:bg-amber-50"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-      {theme === "dark" ? "Light" : "Dark"}
+      {theme === "dark" ? <SunIcon /> : <MoonIcon />}
     </button>
   );
 };
