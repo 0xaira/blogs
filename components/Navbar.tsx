ccff536cf56ef5b061045552841b2fe2320b2524
Aira Jena <airajena.hack@gmail.com>
2024-04-14 10:58:02 +0530

    feat: Add initial navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav className='flex justify-center gap-48 mt-2'>
+            <h1>Aira Jena</h1>
+            <h1>Theme</h1>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
